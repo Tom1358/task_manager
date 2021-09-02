e02bf3eadaa25a94713661ccc2c4b22166c6f21f
Tom1358 <thomasellis358@hotmail.com>
2021-09-02 18:51:05 +0000

    Delete task from the database

--- app.py
+++ app.py
@@ -135,13 +135,17 @@ def edit_task(task_id):
         }
         mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit) # this argument is the 'submit' dictionary on line 128
         flash("Task Successfully Updated")
-        
-        return redirect(url_for("get_tasks"))
 
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({"_id": ObjectId(task_id)})
+    flash("Task Successfully Deleted")
+    return redirect(url_for("get_tasks"))
 
 
 if __name__ == "__main__":
